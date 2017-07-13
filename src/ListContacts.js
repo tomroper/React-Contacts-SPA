4660cd00372855b4408983663d131a1cb409f36c
Tom Roper <tom@Toms-MacBook-Pro.local>
2017-07-12 22:35:33 -0700

    adds Now Showing UI, and option to show all contacts again

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -18,12 +18,16 @@ class ListContacts extends Component {
       this.setState({ query: query.trim() })
   }
 
+  clearQuery = () => {
+    this.setState({ query: '' })
+  }
+
   render() {
     const { contacts, onDeleteContact } = this.props
     const { query } = this.state
 
     let showingContacts
-    if (this.state.query) {
+    if (query) {
       const match = new RegExp(escapeRegExp(this.state.query), 'i')
       showingContacts = contacts.filter((contact) => match.test(contact.name))
     } else {
@@ -42,6 +46,14 @@ class ListContacts extends Component {
             onChange={(event) => this.updateQuery(event.target.value)}
           />
         </div>
+
+        {showingContacts.length !== contacts.length && (
+          <div className='showing-contacts'>
+            <span>Now Showing {showingContacts.length} of {contacts.length}</span>
+            <button onClick={this.clearQuery}>Show All</button>
+          </div>
+        )}
+
         <ol className='contact-list'>
           {showingContacts.map((contact) => (
             <li key={contact.id} className="contact-list-item">
